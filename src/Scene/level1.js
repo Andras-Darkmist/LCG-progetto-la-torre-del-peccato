d15abb084b504918745605b37f57eb2dcc0a09d8
iosonoluigi <luigibombara.lb@gmail.com>
2024-11-12 11:08:12 +0100

    add level1.js

--- src/Scene/level1.js
+++ src/Scene/level1.js
@@ -2,7 +2,7 @@ let img_background;
 
 function preload (s) {
     preload_player (s);
-    img_background = PP.assets.image.load(s, "Assets/Immagini/sfondo.png");
+    img_background = PP.assets.image.load(s, "Assets/Immagini/SFONDO2.png");
 }
 
 function create (s) {

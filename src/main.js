c73537623724dddc82f85bae3b3413661462052b
Andras <andrea.ornaghi@gmail.com>
2024-11-12 10:27:42 +0100

    aggiunti livello e main, rimosso doppione poliphaser

---
+++ src/main.js
@@ -0,0 +1,8 @@
+let config = {
+    canvas_width    : 1280,
+    canvas_heigth   : 720,
+    canvas_id       : "game_area",
+    background_color    : 0x000055,
+    debug_mode: true,
+    gravity: 400,
+}

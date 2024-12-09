b98e34feeecf1e3b63aaba1b14ae40a8d131d13f
Andras <andrea.ornaghi@gmail.com>
2024-12-09 11:02:23 +0100

    Modifica Ghiglio e piattaforme: non più rray ora vanno moltiplicati i file

--- src/ghiglio.js
+++ src/ghiglio.js
@@ -1,29 +1,32 @@
 let img_ghiglio;
-let ghigliottine = [];
-let numero_ghiglio = [];
+let ghiglio;
+let ghiglio_check = true;
 
 function preload_ghiglio (s) {
     img_ghiglio = PP.assets.image.load(s, "assets/immagini/ghiglio.png", 150, 156);
 }
 
 function create_ghiglio (s, x, y){
-    let ghiglio = PP.assets.image.add(s, img_ghiglio, x, y, 0, 0);
-    PP.physics.add(s, ghiglio, PP.physics.type.DYNAMIC);
-    ghiglio.geometry.body_y = -190
-    ghigliottine.push(ghiglio);
+    
+    /*ghiglio.geometry.body_y = -190
+    ghigliottine.push(ghiglio);*/
+        ghiglio = PP.assets.image.add(s, img_ghiglio, x, y, 0, 0);
+        PP.physics.add(s, ghiglio, PP.physics.type.DYNAMIC);
+        PP.physics.set_allow_gravity(ghiglio, false);
+           
 }
 
 function update_ghiglio(s) {
-    for (let i = 0; i < ghigliottine.length; i++) {
-        if (Math.abs((ghigliottine[i].geometry.x) - player.geometry.x) < 10) {
-            numero_ghiglio.push(i);
-            console.log(numero_ghiglio);
-            PP.timers.add_timer(s, 500, scatto_ghiglio(), false);
-        }
+    if (ghiglio.geometry.y <= -170){
+        PP.physics.set_velocity_y(ghiglio, 1500);
     }
+    if (ghiglio.geometry.y >= 180 ){
+        PP.physics.set_velocity_y(ghiglio, -100);    
+    }
+          
 }
 
-function scatto_ghiglio(s){
-    let i = numero_ghiglio.shift();
-    PP.physics.set_velocity_y((ghigliottine[i]), 300);
+function scatto_ghiglio(s)
+{ 
+    PP.physics.set_velocity_y(ghiglio, 1500);
 }
--- src/piatt_move.js
+++ src/piatt_move.js
@@ -1,7 +1,9 @@
 let img_piatt_move1;
+let piatt_move_sing
+/*
 let piatt_move = [];
 let valori_piatt_move = [];
-let vicinanza = [];
+let vicinanza = [];*/
 
 function preload_piatt_move (s) {
     img_piatt_move1 = PP.assets.image.load(s, "assets/immagini/piatt_move.png", 150, 156);
@@ -9,17 +11,29 @@ function preload_piatt_move (s) {
 }
 
 function create_piatt_move (s, x, y){
-    let piatt_move_sing = PP.assets.image.add(s, img_piatt_move1, x, y, 0, 0);
+    /*let*/ piatt_move_sing = PP.assets.image.add(s, img_piatt_move1, x, y, 0, 0);
     PP.physics.add(s, piatt_move_sing, PP.physics.type.DYNAMIC);
     PP.physics.set_immovable(piatt_move_sing, true);
     PP.physics.set_allow_gravity(piatt_move_sing, false);
     PP.physics.set_velocity_y(piatt_move_sing, 0);
     piatt_move.push(piatt_move_sing);
-    vicinanza.push(piatt_move_sing);
+   /* vicinanza.push(piatt_move_sing);*/
 }
 
 function update_piatt_move(s) {
+    if (Math.abs(piatt_move_sing.geometry.x - player.geometry.x < 500)) {
+        if (piatt_move_sing.geometry.y <= -450) {
+            PP.physics.set_velocity_y(piatt_move_sing, 0)
+            PP.timers.add_timer(s, 2000, inizio_discesa, false)
+        }
+        if (piatt_move[0].geometry.y >= 350) {
+            PP.physics.set_velocity_y(piatt_move_sing, 0)
+            PP.timers.add_timer(s, 2000, inizio_salita, false)
+        }
 
+    }
+    
+    /*
     for (let i = 0; i < valori_piatt_move.length; i++) {
         if (Math.abs(valori_piatt_move[i].geometry.x - player.geometry.x < 500)) {
             vicinanza[i] = true;
@@ -57,17 +71,18 @@ function update_piatt_move(s) {
             }
         }
     }
+    */
 }
 
 function inizio_discesa(s){
-    valori_piatt_move.push(0);
+    /*valori_piatt_move.push(0);
     i = valori_piatt_move.shift();
-    console.log(i);
-    PP.physics.set_velocity_y(piatt_move[i], 200)
+    console.log(i);*/
+    PP.physics.set_velocity_y(piatt_move_sing, 200)
 }
 
 function inizio_salita(s){
-    i = valori_piatt_move.shift();
-    console.log(i);
-    PP.physics.set_velocity_y(piatt_move[i], -200)
+    /*i = valori_piatt_move.shift();
+    console.log(i);*/
+    PP.physics.set_velocity_y(piatt_move_sing, -200)
 }

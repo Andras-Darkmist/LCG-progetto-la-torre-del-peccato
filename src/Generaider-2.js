50613a00f591758d4741e22bec5b37eceeb0a130
Andras <andrea.ornaghi@gmail.com>
2024-12-14 15:26:39 +0100

    Completate spine/secondo generatore di casse. Da risolvere problema con i collezionabili non chiaro. Suggerimento cambio level design causa telecamera

---
+++ src/Generaider-2.js
@@ -0,0 +1,37 @@
+let img_generatore2;
+let generatore2;
+let timer_gener2 = false;
+
+function preload_generatore2 (s){
+    img_generatore2 = PP.assets.image.load(s, "assets/immagini/porta1.png", 150, 156);
+}
+
+function create_generatore2 (s){
+    generatore2 = PP.assets.image.add(s, img_generatore2, 3300, 1200, 0, 0);
+    PP.physics.add(s, generatore2, PP.physics.type.DYNAMIC);
+    PP.physics.set_immovable(generatore2, true);
+    PP.physics.set_allow_gravity(generatore2, false);
+}
+
+function update_generatore2 (s) {
+    if (generatore2.geometry.x <= 3300) {
+        PP.physics.set_velocity_x(generatore2, 500);
+    }
+    if (generatore2.geometry.x >= 4000) {
+        PP.physics.set_velocity_x(generatore2, -500);
+    }
+}
+
+function generatore_crea_cassa2 (s) {
+    if (timer_gener2 !=false)
+    {
+        return
+    }
+    spostamento_cassa_generaider2 (s, generatore2.geometry.x, generatore2.geometry.y);
+    timer_gener2 = true;
+    PP.timers.add_timer(s, 1200, reenable_timer2, false);
+}
+
+function reenable_timer2 (s){
+    timer_gener2 = false;
+}
--- src/Scene/level2.js
+++ src/Scene/level2.js
@@ -9,18 +9,25 @@ let pavimento3;
 let scala1;
 let scala2;
 let pedana2;
+let pedana3;
 
 let spine_img;
 
 
 function preload (s) {
+
+    preload_score (s);
+    preload_lettera (s);
+    
     preload_generatore (s);
+    preload_generatore2 (s);
     preload_player (s);
     preload_cassa (s);
     preload_piatt (s);
     preload_Lanciatore(s);
     
     preload_cassa_generaider1 (s);
+    preload_cassa_generaider2 (s);
 
     preload_piatt_move_level2 (s);
 
@@ -35,6 +42,9 @@ function preload (s) {
 }
 
 function create (s) {
+
+    
+
     PP.assets.image.add(s, img_background, 0, 0, 0, 0);
 
     floor = PP.assets.image.add(s, pavimento1, 0, 620, 0, 0);
@@ -78,12 +88,27 @@ function create (s) {
 
     let spine1 = PP.assets.image.add(s, spine_img, 3800, 1700, 0, 0);
     PP.physics.add(s, spine1, PP.physics.type.STATIC);
+
     let spine2 = PP.assets.image.add(s, spine_img, 3546, 1700, 0, 0);
     PP.physics.add(s, spine2, PP.physics.type.STATIC);
     
-    
-    
-    create_piatt_move_level2 (s, 4700, 625);
+    let spine3 = PP.assets.image.add(s, spine_img, 3292, 1700, 0, 0);
+    PP.physics.add(s, spine3, PP.physics.type.STATIC);
+
+    let floor9 = PP.assets.image.add(s, pavimento1, 3134, 1794, 0, 0);
+    PP.physics.add(s, floor9, PP.physics.type.STATIC);
+    let floor10 = PP.assets.image.add(s, pavimento1, 3784, 1794, 0, 0);
+    PP.physics.add(s, floor10, PP.physics.type.STATIC);
+    let floor11 = PP.assets.image.add(s, pavimento1, 2490, 1650, 0, 0);
+    PP.physics.add(s, floor11, PP.physics.type.STATIC);
+
+    create_lettera(s, 2650, 1500);
+    /*
+    for (let i = 0; i < Lettere.length; i++) {
+        PP.physics.add_overlap_f(s, Lettere[i], player, collision_lettera);
+    }
+    */
+    create_piatt_move_level2 (s, 4800, 625);
 
     let scalin6 = PP.assets.image.add(s, scala1, 3000, 470, 0, 0);
     PP.physics.add(s, scalin6, PP.physics.type.STATIC);
@@ -96,13 +121,17 @@ function create (s) {
 
     //funzioni richiamate
     create_generatore (s);
-
+    create_generatore2 (s);
+    
     create_player (s);
     //create_piatt (s);
     create_cassa (s, 300, 200);
     create_cassa_generaider1 (s, 300, 1000);
     create_cassa_generaider1 (s, 300, 1000);
     create_cassa_generaider1 (s, 300, 1000);
+
+    create_cassa_generaider2 (s, 300, 1000);
+    create_cassa_generaider2 (s, 300, 1000);
     
 
     //collider di tutte le cose
@@ -125,6 +154,10 @@ function create (s) {
         PP.physics.add_collider(s, floor6, casse[i]);
         PP.physics.add_collider(s, floor7, casse[i]);
         PP.physics.add_collider(s, floor8, casse[i]);
+        PP.physics.add_collider(s, floor9, casse[i]);
+        PP.physics.add_collider(s, floor10, casse[i]);
+        PP.physics.add_collider(s, floor11, casse[i]);
+
 
         for (let g = 0; g < casse.length; g++) {
             PP.physics.add_collider(s, casse[g], casse[i]);
@@ -132,6 +165,10 @@ function create (s) {
 
         for (let g = 0; g < casse_generaider1.length; g++) {
             PP.physics.add_collider(s, casse_generaider1[g], casse[i]);
+        }
+
+        for (let g = 0; g < casse_generaider2.length; g++) {
+            PP.physics.add_collider(s, casse_generaider2[g], casse[i]);
         }
     }
 
@@ -149,9 +186,35 @@ function create (s) {
         PP.physics.add_collider(s, floor6, casse_generaider1[i]);
         PP.physics.add_collider(s, floor7, casse_generaider1[i]);
         PP.physics.add_collider(s, floor8, casse_generaider1[i]);
+        PP.physics.add_collider(s, floor9, casse_generaider1[i]);
+        PP.physics.add_collider(s, floor10, casse_generaider1[i]);
+        PP.physics.add_collider(s, floor11, casse_generaider1[i]);
 
         for (let g = 0; g < casse.length; g++) {
             PP.physics.add_collider(s, casse_generaider1[g], casse_generaider1[i]);
+        }
+    }
+
+    for (let i = 0; i < casse_generaider2.length; i++) {
+        PP.physics.add_collider_f(s, player, casse_generaider2[i], salto_si);
+        PP.physics.add_collider(s, floor, casse_generaider2[i]);
+        PP.physics.add_collider(s, scalin, casse_generaider2[i]);
+        PP.physics.add_collider(s, scalin3, casse_generaider2[i]);
+        PP.physics.add_collider(s, scalin4, casse_generaider2[i]);
+        PP.physics.add_collider(s, scalin5, casse_generaider2[i]);
+        PP.physics.add_collider(s, floor2, casse_generaider2[i]);
+        PP.physics.add_collider(s, floor3, casse_generaider2[i]);
+        PP.physics.add_collider(s, floor4, casse_generaider2[i]);
+        PP.physics.add_collider(s, floor5, casse_generaider2[i]);
+        PP.physics.add_collider(s, floor6, casse_generaider2[i]);
+        PP.physics.add_collider(s, floor7, casse_generaider2[i]);
+        PP.physics.add_collider(s, floor8, casse_generaider2[i]);
+        PP.physics.add_collider(s, floor9, casse_generaider2[i]);
+        PP.physics.add_collider(s, floor10, casse_generaider2[i]);
+        PP.physics.add_collider(s, floor11, casse_generaider2[i]);
+
+        for (let g = 0; g < casse.length; g++) {
+            PP.physics.add_collider(s, casse_generaider2[g], casse_generaider2[i]);
         }
     }
 
@@ -164,6 +227,7 @@ function create (s) {
     PP.physics.add_collider_f(s, player, scalin6, salto_si);
     PP.physics.add_collider_f(s, player, floor2, salto_si);
     PP.physics.add_collider_f(s, player, floor3, salto_si);
+    PP.physics.add_collider_f(s, player, floor11, salto_si);
     PP.physics.add_collider_f(s, player, floor4, salto_si);
     PP.physics.add_collider_f(s, player, floor5, salto_si);
     PP.physics.add_collider_f(s, player, floor6, salto_si);
@@ -176,6 +240,10 @@ function create (s) {
     pedana2 = PP.shapes.rectangle_add(s, 1200, 939, 150, 40, "0xfbc456", 1);
     PP.physics.add(s, pedana2, PP.physics.type.STATIC);
     PP.physics.add_collider_f(s, player, pedana2, generatore_crea_cassa);
+
+    pedana3 = PP.shapes.rectangle_add(s, 4220, 1650, 150, 40, "0xfbc456", 1);
+    PP.physics.add(s, pedana3, PP.physics.type.STATIC);
+    PP.physics.add_collider_f(s, player, pedana3, generatore_crea_cassa2);
     //PP.physics.add_collider(s, player, piatt1);
     //PP.physics.add_overlap_f(s, player, lanciatore, kill)
 
@@ -186,14 +254,17 @@ function create (s) {
 
     //nemici
 
-    
+    create_score(s);
+
     configure_player_animations(s);
 }
 
 function update (s) {
+    update_score(s);
     player_update(s);
     update_cassa (s);
     update_generatore(s);
+    update_generatore2 (s);
     //update_piatt (s);
     manage_dash(s);
     update_piatt_move_level2(s);

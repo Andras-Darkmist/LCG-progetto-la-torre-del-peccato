4924f1b4373207d5920bc41bfafcf039195e8690
Andras <andrea.ornaghi@gmail.com>
2024-12-30 16:06:51 +0100

    altri sprite aggiunti

--- src/Generaider-2.js
+++ src/Generaider-2.js
@@ -1,13 +1,18 @@
 let img_generatore2;
 let generatore2;
+let img_barra2;
+let barra2;
 let timer_gener2 = false;
 
 function preload_generatore2 (s){
     img_generatore2 = PP.assets.image.load(s, "assets/immagini/Generaider.PNG", 150, 156);
+    img_barra2 = PP.assets.image.load(s, "assets/immagini/Barra_Generaider2.PNG", 150, 156);
 }
 
 function create_generatore2 (s){
+    barra2 = PP.assets.image.add(s, img_barra2, 6070, 1230, 0, 0);
     generatore2 = PP.assets.image.add(s, img_generatore2, 6000, 1230, 0, 0);
+    
     PP.physics.add(s, generatore2, PP.physics.type.DYNAMIC);
     PP.physics.set_immovable(generatore2, true);
     PP.physics.set_allow_gravity(generatore2, false);
